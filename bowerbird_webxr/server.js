a068a42b96436b0fe478936c08943bd6df5ee064
Tavius Koktavy <tavius@nyu.edu>
2024-02-27 12:27:55 -0500

    Test direct folder inclusion

--- bowerbird_webxr/server.js
+++ bowerbird_webxr/server.js
@@ -26,6 +26,7 @@ app.use(cors({
 // Serve static files from 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/assets', express.static(path.join(__dirname, 'dist/assets')));
+app.use('/assets/ui', express.static(path.join(__dirname, 'dist/assets/ui')));
 app.use('/components', express.static(path.join(__dirname, 'dist/components')));
 
 // API Endpoint to provide environment variables
