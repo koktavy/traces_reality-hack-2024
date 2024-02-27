45734ea15a7e6f7ada3fcf7bede38740c7a6085c
Tavius Koktavy <tavius@nyu.edu>
2024-02-27 13:21:11 -0500

    Explicit CORS for static assets

--- bowerbird_webxr/server.js
+++ bowerbird_webxr/server.js
@@ -6,6 +6,14 @@ const app = express();
 // Setting up CORS
 const allowedOrigins = ['https://traces-cyatz.ondigitalocean.app/', 'https://traces.site'];
 
+function setCustomCorsHeader(req, res, next) {
+  const origin = req.get('origin');
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+}
+
 app.use(cors({
   origin: function(origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
@@ -25,9 +33,12 @@ app.use(cors({
 
 // Serve static files from 'dist' directory
 app.use('/', express.static(path.join(__dirname, 'dist')));
-app.use('/assets', express.static(path.join(__dirname, 'dist/assets')));
-app.use('/assets/ui', express.static(path.join(__dirname, 'dist/assets/ui')));
-app.use('/components', express.static(path.join(__dirname, 'dist/components')));
+// app.use('/assets', express.static(path.join(__dirname, 'dist/assets')));
+// app.use('/assets/ui', express.static(path.join(__dirname, 'dist/assets/ui')));
+// app.use('/components', express.static(path.join(__dirname, 'dist/components')));
+app.use('/assets', setCustomCorsHeader, express.static(path.join(__dirname, 'dist/assets')));
+app.use('/assets/ui', setCustomCorsHeader, express.static(path.join(__dirname, 'dist/assets/ui')));
+app.use('/components', setCustomCorsHeader, express.static(path.join(__dirname, 'dist/components')));
 
 // API Endpoint to provide environment variables
 // app.get('/api/get-env-vars', (req, res) => {
