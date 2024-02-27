3f16ae200a7720e08a94dfbc5c66473377c645a1
Tavius Koktavy <tavius@nyu.edu>
2024-02-27 02:11:05 -0500

    Update CORS policy 🙄

--- bowerbird_webxr/server.js
+++ bowerbird_webxr/server.js
@@ -4,19 +4,24 @@ const path = require('path');
 const app = express();
 
 // Setting up CORS
-const allowedOrigins = ['*', 'https://traces-cyatz.ondigitalocean.app/', 'https://traces.site'];
-app.use(
-  cors({
-    origin: function(origin, callback){
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        var msg = 'Origin not allowed';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
+const allowedOrigins = ['https://traces-cyatz.ondigitalocean.app/', 'https://traces.site'];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    // Allow all origins if '*' is present in allowedOrigins
+    if (allowedOrigins.includes('*')) return callback(null, true);
+
+    // Check against allowedOrigins list
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
     }
-  })
-);
+    return callback(null, true);
+  }
+}));
 
 // Serve static files from 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
